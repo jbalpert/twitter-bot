ae61621b5f876c6a193de2fbcf3b66c864b1823f
jbalpert <jbalpert@umich.edu>
2020-07-31 21:40:44 -0400

    removed the twitter keys from repo

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key:         'lsvu3Q7kYShkLFv7co5TiKbg0',
-    consumer_secret:      'vaU6IJLoarsUUkXyTpo9J6TCx4H85LqiWUfnkeRpuQzAsFJ4Lg',
-    access_token:         '1289246931139678208-m4y6QeEPhUhVC58f409X4SVxvKBX2d',
-    access_token_secret:  '3cmMcta3NQymBUZVsEI5Zlihk6bpoHQbhGFGqNNlfSojk'
-}
