003d585a77a80eeca56255110788e4e7be1b0884
jbalpert <jbalpert@umich.edu>
2020-07-31 21:39:14 -0400

    made the jokes actually funny

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
 const Twit = require('twit');
 const config = require('./config');
-const oneLinerJoke = require('one-liner-joke');
+var giveMeAJoke = require('give-me-a-joke');
 
 var T = new Twit(config);
-
-setInterval(twitterBot, 1000*20);
+twitterBot();
+setInterval(twitterBot, 1000*5);
 
 function twitterBot(){
-    // let randomNum = Math.floor(Math.random()*100);
-    // tweet = createTweet("my favorite number is: " + randomNum + "! What's is your favorite?");
-    // postTweet(tweet);
-    
-    var getRandomJoke = oneLinerJoke.getRandomJoke();
-    let text = getRandomJoke.body;
-    console.log('text');
+    giveMeAJoke.getRandomDadJoke (function(joke) {
+        let tweet = {
+            status: joke
+        }
+        
+        postTweet(tweet);
 
-    let tweet = {
-        status: text
-    }
-    
-    postTweet(tweet);
-
-    function postTweet(tweet){
-        T.post('statuses/update', tweet, function(err, data, response) {
-            console.log(tweet.status + " has been posted");
-        });
-    }
+        function postTweet(tweet){
+            T.post('statuses/update', tweet, function(err, data, response) {
+                console.log(tweet.status + " has been posted");
+            });
+        }
+    }); 
 }
 
